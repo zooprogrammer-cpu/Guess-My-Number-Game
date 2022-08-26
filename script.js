fbd76d4069d0738551e353780648af890db74f11
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-08-26 11:51:42 -0500

    css style

--- script.js
+++ script.js
@@ -27,9 +27,13 @@ document.querySelector('.check').addEventListener('click', function(){
     console.log(typeof guess)
 
     if(!guess){
-        document.querySelector('.message').textContent = '⛔️ No Number!'        
+        document.querySelector('.message').textContent = '⛔️ No Number!' 
+        // When Player wins
     } else if (guess === secretNumber){
         document.querySelector('.message').textContent = '🎉 Correct Number'
+        document.querySelector('body').style.backgroundColor = '#60b347'
+        document.querySelector('.number').style.width = '30rem' 
+        //When guess is too high
     } else if (guess > secretNumber){
         if (score >1){
             document.querySelector('.message').textContent = '📈 Too High'
@@ -40,7 +44,7 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.message').textContent = '💥You lost the game!'
             document.querySelector('.score').textContent = 0
         }
-
+        //When guess is too low
     } else if (guess < secretNumber){
         if (score >1){
             document.querySelector('.message').textContent = '📉 Too Low'
