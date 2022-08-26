4f44db7cbb958b929edef5e1eb2e391a0941045b
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-08-26 11:08:28 -0500

    Too high and too low game

--- script.js
+++ script.js
@@ -13,8 +13,45 @@
 // console.log(document.querySelector('.guess').value);
 
 //clicking button runs the function
+
+const secretNumber = Math.trunc(Math.random()*20+1)
+
+let score = 20
+
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.score').textContent = score
+
 document.querySelector('.check').addEventListener('click', function(){
-    console.log(document.querySelector('.guess').value)
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(guess);
+    console.log(typeof guess)
+
+    if(!guess){
+        document.querySelector('.message').textContent = '⛔️ No Number!'        
+    } else if (guess === secretNumber){
+        document.querySelector('.message').textContent = '🎉 Correct Number'
+    } else if (guess > secretNumber){
+        if (score >1){
+            document.querySelector('.message').textContent = '📈 Too High'
+            score = score -1
+            document.querySelector('.score').textContent = score
+        }
+        else {
+            document.querySelector('.message').textContent = '💥You lost the game!'
+            document.querySelector('.score').textContent = 0
+        }
+
+    } else if (guess < secretNumber){
+        if (score >1){
+            document.querySelector('.message').textContent = '📉 Too Low'
+            score = score -1
+            document.querySelector('.score').textContent = score
+        }
+        else{
+            document.querySelector('.message').textContent = '💥You lost the game!'
+            document.querySelector('.score').textContent = 0
+        }
+    }
 })
 
 
