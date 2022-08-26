21f25b8017193948a13d39be49b18c847901328f
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-08-26 10:16:25 -0500

    querySelector basics

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent)
+
+// document.querySelector('.message').textContent = 'Correct Number 🎉'
+// console.log(document.querySelector('.message').textContent)
+
+
+// document.querySelector('.number').textContent = 13
+// document.querySelector('.score').textContent = 15
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+//clicking button runs the function
+document.querySelector('.check').addEventListener('click', function(){
+    console.log(document.querySelector('.guess').value)
+})
+
+
